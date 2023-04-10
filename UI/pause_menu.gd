47af8e3993e67dd32f4612485aab51772038c2b6
Ghostitto <fuentesbenjamin11@gmail.com>
2023-04-09 21:04:33 -0400

    ui
    main ui, select ui and pause menu

---
+++ UI/pause_menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+@onready var Return = %Return
+@onready var Retry = %Retry
+@onready var Main_menu = %"Main_menu"
+
+
+func _ready():
+	Return.pressed.connect(_on_Return_pressed)
+	Retry.pressed.connect(_on_Retry_pressed)
+	Main_menu.pressed.connect(_on_Main_menu_pressed)
+	hide()
+
+func _input(event):
+	if event.is_action_pressed("Pause"):
+		show()
+		get_tree().paused = true
+
+func _on_Return_pressed():
+	hide()
+	get_tree().paused = false
+
+func _on_Retry_pressed():
+	get_tree().reload_current_scene()
+	get_tree().paused = false
+
+func _on_Main_menu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://UI/main_ui.tscn")
