06c8cee0e23ba34aaf9b37c6c935d2b5890774ec
SantiagoDelRio <santiagodelrioahumada@gmail.com>
2023-07-18 16:08:04 -0400

    Added animations and pivot

--- Scripts/follower.gd
+++ Scripts/follower.gd
@@ -8,6 +8,8 @@ const GRAVITY = 1500
 @onready var target_detection = $target_detection
 @onready var animation_tree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/playback")
+@onready var pivot = $Pivot
+@onready var hit_hitbox = $Pivot/hit_area/hit_hitbox
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -17,12 +19,17 @@ var awake = false
 var target = null
 var overlapping = []
 
+func _ready():
+	animation_tree.active = true
+	
+
 func _physics_process(delta):
 	move_and_slide()
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 	
 	if awake:
+		#Mueve al NPC
 		if target_chase:
 			velocity.x = position.direction_to(target.position).x * SPEED
 		else:
@@ -30,12 +37,24 @@ func _physics_process(delta):
 		if target != null:
 			if (target.position.y - position.y) < -80 and is_on_floor():
 				velocity.y = JUMP_VELOCITY
+				
+		#Animaciones
+		if abs(velocity.x) > 100:
+			playback.travel("run") 
+		else:
+			playback.travel("buff_idle")
+		if velocity.x != 0:
+			pivot.scale.x = sign(velocity.x)
+			
+		#Golpes
+		
+		
+		
 
 func launch(v):
 	playback.travel("Transform")
 	await get_tree().create_timer(2).timeout
 	awake = true
-
 
 func _on_area_2d_body_entered(body):
 	if (body.name == "Player" or body.name == "Player2") and target == null:
@@ -56,6 +75,7 @@ func _on_area_2d_body_exited(body):
 				target = bodi
 				target_chase = true
 		
-		
-		
-		
+
+func _on_hit_detection_body_entered(body):
+	if body.name == "Player":
+		playback.travel("hit")
